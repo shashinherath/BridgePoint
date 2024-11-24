ec579bcabb6fd662e178605baf4c6907548887b5
shashinherath <shashin.malinda@gmail.com>
2024-11-24 23:12:15 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Bridge Point</h1>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </>
   );
 }
