1af14548abd28805373addb57229d13d56cf0645
VinushiRanshila <139769880+VinushiRanshila@users.noreply.github.com>
2024-11-29 11:42:11 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,19 +9,19 @@ function App() {
     <div className="w-1/2 bg-white flex flex-col justify-center items-center">
       <img src={Logo} alt="Bridge Point Logo" className="w-32 mb-4" />
       <h1 className="text-3xl font-bold mb-2">Welcome to BridgePoint</h1>
-      <p className="text-center text-gray-600 mb-6">
+      <p className="text-center text-red-600 mb-6">
         Log in with your provided credentials to unlock a world of community connections and endless opportunities with BridgePoint!
       </p>
 
       <form className="w-2/3">
         <div className="mb-4">
           <label className="block text-gray-700 mb-2">Username</label>
-          <input type="text" className="w-full p-3 border rounded-lg" placeholder="Enter your username"/>
+          <input type="text" className="w-full p-3 border rounded-lg focus:ring-orange-500" placeholder="Enter your username"/>
         </div>
 
         <div className="mb-4">
           <label className="block text-gray-700 mb-2">Password</label>
-          <input type="password" className="w-full p-3 border rounded-lg" placeholder="Enter your password"/>
+          <input type="password" className="w-full p-3 border rounded-lg focus:ring-orange-500" placeholder="Enter your password"/>
         </div>
 
         <div className="flex items-center mb-4">
@@ -29,7 +29,7 @@ function App() {
           <label className="text-gray-700">Remember me</label>
         </div>
 
-        <button className="w-full p-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
+        <button className="w-full p-3 bg-orange-600 text-white rounded-lg hover:bg-orange-700">
           Log in
         </button>
       </form>
