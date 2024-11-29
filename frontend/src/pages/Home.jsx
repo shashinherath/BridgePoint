cfe0668e0bcde718c52094d3a294ca5272713d2b
shashinherath <shashin.malinda@gmail.com>
2024-11-29 12:02:19 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,12 +3,14 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import HomeList from "../components/HomeList";
 import Hero from "../components/Hero";
+import AdBanner from "../components/AdBanner";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Navbar />
       <Hero />
+      <AdBanner />
       <div className="flex flex-col justify-center items-center flex-grow">
         <HomeList bgColor="#DDFBFC" circleColor="#8EBD8A" />
         <HomeList bgColor="#FCDDDD" circleColor="#C57474" />
