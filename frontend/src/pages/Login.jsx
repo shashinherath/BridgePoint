eab49292c9674b3b010c228a9bce642fc15f4d4c
VinushiRanshila <139769880+VinushiRanshila@users.noreply.github.com>
2024-12-05 10:26:10 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../assets/logo/Logo.png'
+import Logo2 from '../assets/logo/Logo2.png'
 import background from '../assets/images/background.jpg'
 
 function App() {
@@ -7,21 +7,21 @@ function App() {
     <div className="flex h-screen">
     {/* Left Section */}
     <div className="w-1/2 bg-white flex flex-col justify-center items-center">
-      <img src={Logo} alt="Bridge Point Logo" className="w-32 mb-4" />
-      <h1 className="text-3xl font-bold mb-2">Welcome to BridgePoint</h1>
-      <p className="text-center text-red-600 mb-6">
+      <img src={Logo2} alt="Bridge Point Logo" className="w-32 mb-4" />
+      <h1 className="text-4xl font-bold mb-2">Welcome to BridgePoint</h1>
+      <p className="text-center text-gray-700 text-lg md:text-xl px-3">
         Log in with your provided credentials to unlock a world of community connections and endless opportunities with BridgePoint!
       </p>
 
       <form className="w-2/3">
-        <div className="mb-4">
-          <label className="block text-gray-700 mb-2">Username</label>
-          <input type="text" className="w-full p-3 border rounded-lg focus:ring-orange-500" placeholder="Enter your username"/>
+        <div className="mb-4 mt-4">
+          <label className="text-gray-600 text-lg font-medium">Username</label>
+          <input type="text" className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 " placeholder="Enter your username"/>
         </div>
 
         <div className="mb-4">
-          <label className="block text-gray-700 mb-2">Password</label>
-          <input type="password" className="w-full p-3 border rounded-lg focus:ring-orange-500" placeholder="Enter your password"/>
+          <label className="text-gray-600 text-lg font-medium">Password</label>
+          <input type="password" className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 " placeholder="Enter your password"/>
         </div>
 
         <div className="flex items-center mb-4">
