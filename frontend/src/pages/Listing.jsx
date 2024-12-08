1775d6e11a90be6f53da8556c24d0c30b48cdcdf
shashinherath <shashin.malinda@gmail.com>
2024-12-08 14:25:26 +0530

    Update Listing.jsx

--- frontend/src/pages/Listing.jsx
+++ frontend/src/pages/Listing.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import BrowseService from "../components/BrowseService";
+import BrowseList from "../components/BrowseList";
 
 export default function Listing() {
   return (
@@ -9,6 +10,7 @@ export default function Listing() {
       <Navbar />
       <div className="flex flex-col justify-center items-center flex-grow">
         <BrowseService />
+        <BrowseList />
       </div>
       <Footer />
     </div>
