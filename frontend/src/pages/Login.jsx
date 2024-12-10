833e9ea9bbf0c5ef9764a6fbe66b98fe8aa131db
shashinherath <shashin.malinda@gmail.com>
2024-12-10 21:53:23 +0530

    Fix Register and Login pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,46 +1,67 @@
-import React from 'react'
-import Logo2 from '../assets/logo/Logo2.png'
-import background from '../assets/images/background.jpg'
+import React from "react";
+import Logo2 from "../assets/logo/Logo2.png";
+import background from "../assets/images/Background.jpg";
 
 function App() {
   return (
     <div className="flex h-screen">
-    {/* Left Section */}
-    <div className="w-1/2 bg-white flex flex-col justify-center items-center">
-      <img src={Logo2} alt="Bridge Point Logo" className="w-32 mb-4" />
-      <h1 className="text-4xl font-bold mb-2">Welcome to BridgePoint</h1>
-      <p className="text-center text-gray-700 text-lg md:text-xl px-3">
-        Log in with your provided credentials to unlock a world of community connections and endless opportunities with BridgePoint!
-      </p>
-
-      <form className="w-2/3">
-        <div className="mb-4 mt-4">
-          <label className="text-gray-600 text-lg font-medium">Username</label>
-          <input type="text" className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 " placeholder="Enter your username"/>
+      {/* Left Section */}
+      <div className="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:w-1/2 lg:flex-none lg:px-20 xl:px-24">
+        <div className="flex justify-center">
+          <img src={Logo2} alt="Bridge Point Logo" className="w-52 mb-4" />
         </div>
+        <h1 className="text-center text-4xl font-bold mb-2">
+          Welcome to BridgePoint
+        </h1>
+        <p className="text-center text-gray-700 text-lg md:text-xl px-3">
+          Log in with your provided credentials to unlock a world of community
+          connections and endless opportunities with BridgePoint!
+        </p>
 
-        <div className="mb-4">
-          <label className="text-gray-600 text-lg font-medium">Password</label>
-          <input type="password" className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 " placeholder="Enter your password"/>
-        </div>
+        <form>
+          <div className="mb-4 mt-4">
+            <label className="text-gray-600 text-base font-medium">
+              Username
+            </label>
+            <input
+              type="text"
+              className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+              placeholder="Enter your username"
+            />
+          </div>
 
-        <div className="flex items-center mb-4">
-          <input type="checkbox" className="mr-2" />
-          <label className="text-gray-700">Remember me</label>
-        </div>
+          <div className="mb-4">
+            <label className="text-gray-600 text-base font-medium">
+              Password
+            </label>
+            <input
+              type="password"
+              className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+              placeholder="Enter your password"
+            />
+          </div>
 
-        <button className="w-full p-3 bg-orange-600 text-white rounded-lg hover:bg-orange-700">
-          Log in
-        </button>
-      </form>
+          <div className="flex items-center mb-4">
+            <input type="checkbox" className="mr-2" />
+            <label className="text-gray-700">Remember me</label>
+          </div>
+
+          <button className="w-full p-3 bg-orange-600 text-white text-base font-bold rounded-lg hover:bg-orange-700">
+            Log in
+          </button>
+        </form>
+      </div>
+
+      {/* Right Section */}
+      <div className="relative hidden w-0 lg:w-1/2 lg:block flex-1">
+        <img
+          src={background}
+          alt="Abstract Tech Background"
+          className="h-full w-full object-cover"
+        />
+      </div>
     </div>
-
-    {/* Right Section */}
-    <div className="w-1/2 bg-gray-800 flex justify-center items-center">
-      <img src={background}  alt="Abstract Tech Background" className="h-full w-full object-cover opacity-70"/>
-    </div>
-  </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,99 +1,235 @@
-import React, { useState } from 'react';
-import Logo2 from '../assets/logo/Logo2.png'
-import background from '../assets/images/background.jpg'
-
+import React, { useState } from "react";
+import Logo2 from "../assets/logo/Logo2.png";
+import background from "../assets/images/Background.jpg";
 
 const Register = () => {
   const [isServiceProvider, setIsServiceProvider] = useState(false);
+
   return (
-  <div className="flex h-screen">
-    {/* Left Section */}
-    <div className="w-1/2 bg-white flex flex-col justify-center items-center">
-      <img src={Logo2} alt="Bridge Point Logo" className="w-32 mb-4" />
-      <p className="text-center text-gray-700 text-lg md:text-xl px-3">
-      Unlock your potential at BridgePoint A vibrant community of growth and endless opportunities!
-      </p>
-      <div className="flex justify-center items-center mt-4">
-      <span className="mr-2 text-black-700">Service Provider</span>
-      <label className="relative inline-flex items-center cursor-pointer">
-              <input type="checkbox" className="sr-only" checked={isServiceProvider} onChange={() => setIsServiceProvider(!isServiceProvider)}/>
-              <div className="w-11 h-6 bg-gray-200 rounded-full peer bg-blue-400">
-                <div className={`w-5 h-5 bg-blue-700 rounded-full transition-all transform ${isServiceProvider ? 'translate-x-5' : ''}`}></div>
-              </div>
-            </label>
-            <span className="ml-2 text-black-700">Student</span>
-          </div>
-          <h2 className="text-center text-lg font-semibold mt-6">
-            {isServiceProvider ? 'Service Provider' : 'Student'} Registration Form
-          </h2>
-          <form className="mt-4">
-          <div className="grid grid-cols-2 gap-4">
-          
-          <div>
-          <label className="text-gray-600 text-base font-medium">Full Name</label>
-            <input type="text"  placeholder="First Name" className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500" />    
-          </div>
-          <div>
-            <input type="text" placeholder="Last Name" className="mt-7 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"/>   
-          </div>
-
-         
-          <div>
-          <label className="text-gray-600 text-base font-medium">Mobile Number</label>
-            <input type="tel" placeholder="+94" className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"/>
-          </div>
-
-        
-          <div>
-          <label className="text-gray-600 text-base font-medium">email</label>
-            <input type="email" placeholder="ex: ict21067@std.uwu.ac.lk" className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"/>
-          </div>
-
-         
-          <div>
-          <label className="text-gray-600 text-base font-medium">Password</label>
-            <input type="password" className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"/>
-          </div>
-
-         
-          <div>
-          <label className="text-gray-600 text-base font-medium">Confirm Password</label>
-            <input type="password" className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"/>
-          </div>
+    <div className="flex h-screen">
+      {/* Left Section */}
+      <div className="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:w-1/2 lg:flex-none lg:px-20 xl:px-24 overflow-y-auto">
+        <div className="flex justify-center">
+          <img src={Logo2} alt="Bridge Point Logo" className="w-52 mb-4" />
         </div>
+        <p className="text-center text-gray-700 text-lg md:text-xl px-3">
+          {isServiceProvider
+            ? "Join BridgePoint as a seller and connect with a thriving community eager for your services. Register today and expand your opportunities!"
+            : "Unlock your potential at BridgePoint - A vibrant community of growth and endless opportunities!"}
+        </p>
+        <div className="flex justify-center items-center mt-4">
+          <span className="mr-2 text-orange-900 text-base font-medium">
+            Student
+          </span>
+          <label className="relative inline-flex items-center cursor-pointer">
+            <input
+              type="checkbox"
+              className="sr-only"
+              checked={isServiceProvider}
+              onChange={() => setIsServiceProvider(!isServiceProvider)}
+            />
+            <div className="w-11 h-6 bg-gray-300 rounded-full peer">
+              <div
+                className={`w-6 h-6 bg-orange-600 rounded-full transition-all transform ${
+                  isServiceProvider ? "translate-x-5" : ""
+                }`}
+              ></div>
+            </div>
+          </label>
+          <span className="ml-2 text-orange-900 text-base font-medium">
+            Service Provider
+          </span>
+        </div>
+        <h2 className="text-center text-lg font-semibold mt-6">
+          {isServiceProvider ? "Service Provider" : "Student"} Registration Form
+        </h2>
 
-        
-        <div className="flex flex-col mt-4">
-  <div className="flex justify-between items-center">
-    <div>
-      <label className="block text-sm font-medium text-gray-700">
-        Upload Student Verification
-      </label>
-      <span className="text-sm text-black-200">Student ID or other document</span>
-    </div>
-    <div className="relative">
-      <input type="file" className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"/>
-      <div className="flex items-center justify-center px-6 py-2 mr-28 border border-gray-500 rounded-lg bg-gray-100 text-gray-800 cursor-pointer">
-        <span>upload</span>
+        <form className="mt-4">
+          {!isServiceProvider ? (
+            // Student Registration Form
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <label className="text-gray-600 text-base font-medium">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  placeholder="First Name"
+                  className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Last Name"
+                  className="mt-7 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+                />
+              </div>
+
+              <div>
+                <label className="text-gray-600 text-base font-medium">
+                  Mobile Number
+                </label>
+                <input
+                  type="tel"
+                  placeholder="+94"
+                  className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+                />
+              </div>
+
+              <div>
+                <label className="text-gray-600 text-base font-medium">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  placeholder="ex: ict21067@std.uwu.ac.lk"
+                  className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+                />
+              </div>
+
+              <div>
+                <label className="text-gray-600 text-base font-medium">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+                />
+              </div>
+
+              <div>
+                <label className="text-gray-600 text-base font-medium">
+                  Confirm Password
+                </label>
+                <input
+                  type="password"
+                  className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+                />
+              </div>
+            </div>
+          ) : (
+            // Service Provider Registration Form
+            <>
+              <div className="mb-4">
+                <label className="text-gray-600 text-base font-medium">
+                  Company Name
+                </label>
+                <input
+                  type="text"
+                  className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-600 text-base font-medium">
+                  Address
+                </label>
+                <input
+                  type="text"
+                  className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+                />
+
+                <div className="grid grid-cols-2 gap-4 mt-2">
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="City"
+                      className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="State / Province"
+                      className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="mb-4">
+                <label className="text-gray-600 text-base font-medium">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  placeholder="myname@example.com"
+                  className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+                />
+              </div>
+
+              <div className="grid grid-cols-2 gap-4">
+                <div>
+                  <label className="text-gray-600 text-base font-medium">
+                    Mobile Number
+                  </label>
+                  <input
+                    type="text"
+                    className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+                  />
+                </div>
+                <div>
+                  <div className="mb-4">
+                    <label className="text-gray-600 text-base font-medium">
+                      Provided Service
+                    </label>
+                    <select
+                      id="service"
+                      className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+                    >
+                      <option>Please Select</option>
+                      <option>Food</option>
+                      <option>Accommodation</option>
+                      <option>Rides</option>
+                      <option>Guide</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div className="grid grid-cols-2 gap-4">
+                <div>
+                  <label className="text-gray-600 text-base font-medium">
+                    Password
+                  </label>
+
+                  <input
+                    type="password"
+                    className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+                  />
+                </div>
+                <div>
+                  <label className="text-gray-600 text-base font-medium">
+                    Confirm Password
+                  </label>
+
+                  <input
+                    type="password"
+                    className="mt-1 block w-full px-4 py-2 border border-gray-500 rounded-lg outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+                  />
+                </div>
+              </div>
+            </>
+          )}
+
+          <button
+            type="submit"
+            className="w-full mt-6 bg-orange-600 text-white text-base font-bold p-3 px-4 rounded-lg hover:bg-orange-700"
+          >
+            Register
+          </button>
+        </form>
+      </div>
+
+      {/* Right Section */}
+      <div className="relative hidden w-0 lg:w-1/2 lg:block flex-1">
+        <img
+          src={background}
+          alt="background image"
+          className="h-full w-full object-cover"
+        />
       </div>
     </div>
-  </div>
-</div>
+  );
+};
 
-   
-        <button type="submit" className="w-full mt-6 bg-orange-600 text-white py-2 px-4 rounded-lg hover:bg-orange-700">
-          Register
-        </button>
-      </form>
-          
-        </div>
-        <div className="w-1/2 bg-gray-800 flex justify-center items-center">
-        <img src={background} alt="background image" className="h-full w-full object-cover opacity-70"/>
-</div>
-</div>
-
-       )
-       
-}
-
-export default Register
+export default Register;
