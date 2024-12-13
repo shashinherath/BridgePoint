508e81578f2dfb418a3d9b437165c0150e53391e
shashinherath <shashin.malinda@gmail.com>
2024-12-13 19:15:21 +0530

    Update BrowseList.jsx

--- frontend/src/components/BrowseList.jsx
+++ frontend/src/components/BrowseList.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import ListView from "./ListView";
 
 export default function BrowseList() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const openPopup = () => {
+    setShowPopup(true);
+  };
+
   return (
     <div className="flex flex-col justify-start w-full px-20">
       <h1 className="text-2xl font-bold">Popular near you</h1>
 
       <div className="xl:lg:md:sm:grid xl:lg:md:sm:grid-cols-4 xl:lg:md:sm:justify-items-center flex flex-wrap justify-start py-5">
-        {Array.from({ length: 10 }).map((_, i) => (
-          <div className="bg-white w-72 h-64 m-4 rounded-lg shadow-xl">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <div
+            key={index}
+            className="bg-white w-72 h-64 m-4 rounded-lg shadow-xl"
+          >
             <img
               src="https://via.placeholder.com/150"
               alt="food"
@@ -15,10 +25,13 @@ export default function BrowseList() {
             />
             <div className="p-4">
               <h1 className="text-xl font-semibold">CDK</h1>
-              <p className="text-sm text-gray-500">Vegitable Rice</p>
+              <p className="text-sm text-gray-500">Vegetable Rice</p>
               <div className="mt-4 flex justify-between items-center">
                 <p className="text-xl font-bold">Rs. 150</p>
-                <button className="bg-orange-800 text-white px-4 py-2 rounded-md">
+                <button
+                  className="bg-orange-800 text-white px-4 py-2 rounded-md"
+                  onClick={openPopup}
+                >
                   View
                 </button>
               </div>
@@ -26,6 +39,7 @@ export default function BrowseList() {
           </div>
         ))}
       </div>
+      {showPopup && <ListView setShowPopup={setShowPopup} />}
     </div>
   );
 }
