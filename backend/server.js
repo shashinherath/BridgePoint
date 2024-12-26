8ac81e3a1d012e5f85eeeb943be227aef23796b5
shashinherath <shashin.malinda@gmail.com>
2024-12-26 22:57:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/auth");
 const serviceRoutes = require("./routes/services");
@@ -16,6 +17,16 @@ const app = express();
 
 // Middleware to parse JSON requests
 app.use(express.json());
+
+// Custom CORS configuration
+const corsOptions = {
+  origin: process.env.Frontend_URL,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 // Routes
 app.use("/api/auth", authRoutes);
