bd57f154ab9c46da641ac7c9da3cd0c3b9152902
shashinherath <shashin.malinda@gmail.com>
2025-01-13 23:40:51 +0530

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,22 @@
 const express = require("express");
-const { deleteUser } = require("../controllers/userController");
+const { getUser, updateUser } = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware");
+const upload = require("../middleware/uploadMiddleware");
 const router = express.Router();
 
-// @route   DELETE /api/user/delete
-// @desc    Delete an user
+// @route   GET /api/user/getuser
+// @desc    Get user details
 // @access  Private
-router.delete("/delete", authMiddleware, deleteUser);
+router.get("/getuser", authMiddleware, getUser);
+
+// @route   PUT /api/user/updateuser
+// @desc    Update user details
+// @access  Private
+router.put(
+  "/updateuser",
+  authMiddleware,
+  upload.single("profileImage"),
+  updateUser
+);
 
 module.exports = router;
