6b1de8be2633e0861cb6dc38dd477bc397ac3b35
shashinherath <shashin.malinda@gmail.com>
2025-01-19 11:58:00 +0530

    Update HomeList.jsx

--- frontend/src/components/HomeList.jsx
+++ frontend/src/components/HomeList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function HomeList({ bgColor, circleColor, array, title }) {
   return (
@@ -9,20 +10,25 @@ export default function HomeList({ bgColor, circleColor, array, title }) {
       <h1 className="font-semibold text-xl pb-4 ml-10">{title}</h1>
       <div className="flex flex-wrap justify-center space-x-4">
         {array.map((item, i) => (
-          <div key={i}>
-            <div
-              className="rounded-full h-24 w-24 sm:h-32 sm:w-32 md:h-40 md:w-40 lg:h-48 lg:w-48 bg-white flex items-center justify-center"
-              style={{ backgroundColor: circleColor }}
-            >
-              <img
-                src={item.image}
-                alt={item.name}
-                className="h-16 w-16 sm:h-24 sm:w-24 md:h-32 md:w-32 lg:h-40 lg:w-40"
-              />
-            </div>
-            <p className="flex justify-center font-bold text-sm pt-2">
-              {item.name}
-            </p>
+          <div
+            key={i}
+            className="hover:scale-105 transition-transform duration-300"
+          >
+            <Link to={item.url}>
+              <div
+                className="rounded-full h-24 w-24 sm:h-32 sm:w-32 md:h-40 md:w-40 lg:h-48 lg:w-48 bg-white flex items-center justify-center"
+                style={{ backgroundColor: circleColor }}
+              >
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className="h-16 w-16 sm:h-24 sm:w-24 md:h-32 md:w-32 lg:h-40 lg:w-40"
+                />
+              </div>
+              <p className="flex justify-center font-bold text-sm pt-2 hover:text-gray-500 transition-colors duration-300">
+                {item.name}
+              </p>
+            </Link>
           </div>
         ))}
       </div>
