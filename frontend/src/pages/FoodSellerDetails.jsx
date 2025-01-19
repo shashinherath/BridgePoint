2e3187d7535619d642e7017b4c6f26862afc223b
shashinherath <shashin.malinda@gmail.com>
2025-01-19 15:10:07 +0530

    Update FoodSellerDetails.jsx

--- frontend/src/pages/FoodSellerDetails.jsx
+++ frontend/src/pages/FoodSellerDetails.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import SellerProfile from "../components/SellerProfile";
+import { useState } from "react";
 
 const FoodSellerDetails = () => {
-    return (
-        
-        <div className="flex flex-col">
-            <Navbar />
-            <SellerProfile />
-            <Footer />
-        </div>
-    );
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <div className="flex flex-col">
+      <Navbar onSearch={setSearchTerm} searchItem={searchTerm} />
+      <SellerProfile />
+      <Footer />
+    </div>
+  );
 };
 
 export default FoodSellerDetails;
